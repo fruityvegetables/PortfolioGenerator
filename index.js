945d36911a9764c939483a0f573a796c60f88b06
fruityvegetables <gg.troyer@gmail.com>
2020-04-09 11:11:30 -0500

    MVP, completed work, just ugly

--- index.js
+++ index.js
@@ -15,45 +15,47 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}`;
-
-
-    axios.get(queryUrl).then(function(res) {
-        const avatar = res.data.avatar_url;
-        const email = res.data.email;
-    //  console.log(res);
-    //   const repoNames = res.data.map(function(repo) {
-    //     return repo.name;
-      }).then(
         inquirer
         .prompt([{
-          message: "Enter the name of your repo: ",
-          name: "userRepo"
+            message: "Enter your name: ",
+            name: "userName" 
+        },
+        {
+          message: "Seeeennnnd me, your location: ",
+          name: "userLoc"
         },
         { 
-            message: "blah",
-            name: "blah"
-        }])
-        .then(function({ userRepo, blah }){
-            //console.log(userRepo,blah);
-            let array = [userRepo, blah];
+            message: "Tell me a little about yourself!: ",
+            name: "userBio"
+        },
+        {
+            message: "Enter your GitHub URL:",
+            name: "userGit" 
+        },
+        {
+            message: "Enter your LinkedIn URL:",
+            name: "userLinked" 
+        }
+    ])
+        .then(function({ userName, userLoc, userBio, userGit, userLinked }){
+            //console.log( userName, userLoc, userBio, userGit, userLinked);
+            let array = [ `<!DOCTYPE html>
+            <html lang="en">
+            <h1>Hi! This portfolio represents: ` + userName + `</h1>`, 
+            `<header>` + `<h2>Located in: ` + userLoc + `</h2>` + `</header>`, 
+            `<body>` + `<h3>A short Biography: ` + userBio + `</h3>`, 
+            `<a href="`+userGit+`">Link to my GitHub!</a>`, 
+            `<a href="`+userLinked+`">Link to my LinkedIn!</a>`,
+            `</body></html>`];
             const content = array.join("\n");
-            fs.writeFile("README.md", content, function(err){
+            fs.writeFile("index.html", content, function(err){
                 if (err){ 
                     throw err;
                 }
                 console.log("done");
             })
-        })
+        });
         
-      )
     //   inquirer
     //   .prompt({
     //     type: "list",
@@ -71,4 +73,3 @@ inquirer
     //     //console.log(`Saved ${repoNames.length} repos`);
     //   });
     // });
-  });
